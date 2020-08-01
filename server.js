f1afd902c9d5481a851a6dc15197dcb3a285411d
Adnan Reza <adnan.reza@outlook.com>
2020-08-01 01:35:02 -0700

    Responded with JSON data and added headers to response

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require('http');
 
+const todos = [
+  { id: 1, text: 'Todo One' },
+  { id: 2, text: 'Todo Two' },
+];
+
 const server = http.createServer((req, res) => {
-  const { headers, url, method } = req;
-  console.log(headers, url, method);
-  res.end();
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('X-Powered-By', 'Node.js');
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos,
+    })
+  );
 });
 
 const PORT = 5000;
